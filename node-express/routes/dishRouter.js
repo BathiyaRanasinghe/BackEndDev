ae61e366f02f63ccc55ce8d286494a52c1f86e3b
bathiyaranasinghe <bar407@usask.ca>
2022-05-10 22:16:44 -0600

    Assignment 1

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
@@ -23,6 +22,27 @@ dishRouter.route('/')
     })
     .delete((req, res, next) => {
         res.end('Deleting all dishes');
+    });
+
+dishRouter.route('/:dishId')
+    .all(function (req, res, next) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+
+    .get(function (req, res, next) {
+        res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+    })
+
+    .put(function (req, res, next) {
+        res.write('Updating the dish: ' + req.params.dishId + '\n');
+        res.end('Will update the dish: ' + req.body.name +
+            ' with details: ' + req.body.description);
+    })
+
+    .delete(function (req, res, next) {
+        res.end('Deleting dish: ' + req.params.dishId);
     });
 
 module.exports = dishRouter;
