04a6b4e7d0233cb77b7f27f2be0fb7d3a266bd85
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2022-05-15 12:42:09 -0600

    Passport JWT Bug Fix

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -1,34 +1,32 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
 
 const Dishes = require('./models/dishes');
 
-// Connection URL
-const url = 'config.mongoUrl';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
-  useMongoClient: true,
-  /* other options */
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 connect.then((db) => {
-  console.log("Connected correctly to server");
+  console.log('Connected correctly to server');
 }, (err) => { console.log(err); });
 
 var app = express();
@@ -37,31 +35,25 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
+// app.use(passport.session());
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
 // error handler
