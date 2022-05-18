dab52b4b599b3159e1cfca80f38f5e1218c59f74
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2022-05-18 15:52:17 -0600

    Assignment 4

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -14,7 +14,8 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-const uploadRouter = require('./routes/uploadRouter');
+var uploadRouter = require('./routes/uploadRouter');
+var favouriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -22,6 +23,8 @@ const Dishes = require('./models/dishes');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 connect.then((db) => {
@@ -60,6 +63,7 @@ app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites', favouriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
